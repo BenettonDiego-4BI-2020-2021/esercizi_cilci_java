6e11a42665d226641e04759341f40c97d13e123f
18535 <18535@W10STAT-7-155.EDU.MARCONI>
2020-11-10 09:19:05 +0100

    PersoneAmmalate_02

---
+++ src/eser_cicli/PersoneAmmalate_02.java
@@ -0,0 +1,14 @@
+package eser_cicli;
+
+public class PersoneAmmalate_02 {
+    
+    public static void main(String[] args) {
+        int ammalati=1000,giorni=0;
+        double guariti_percentuale=20,guariti_numero=((double)ammalati/100)*guariti_percentuale;
+        while(ammalati>100){
+            ammalati-=guariti_numero;
+            giorni+=1;
+        }
+        System.out.println("ci sono voluti "+giorni+" giorni affinché il numero di ammalati fosse minore di 100");
+    }
+}
