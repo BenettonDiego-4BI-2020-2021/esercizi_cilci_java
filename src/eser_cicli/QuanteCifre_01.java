ea920bf40b4559ad0c3af950aa555ab54d9eb19d
Utente <Utente@DB841-PC>
2020-11-15 17:48:01 +0100

    QuanteCifre_01

---
+++ src/eser_cicli/QuanteCifre_01.java
@@ -0,0 +1,16 @@
+package eser_cicli;
+
+import java.util.Scanner;
+
+public class QuanteCifre_01 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int num,pari=0,dispari=0,buffer;
+        String numero;
+        System.out.println("inserire un numero");
+        num= inp.nextInt();
+        numero = Integer.toString(num);
+        String numeri[] = numero.split("");
+        System.out.println("il numero è composto da "+numeri.length+" cifre");
+    }
+}
