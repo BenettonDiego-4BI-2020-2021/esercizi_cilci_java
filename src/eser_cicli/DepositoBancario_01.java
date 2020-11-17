78a15dcd7a2ec91bd005ff1c6b7b43e1cd06a8b1
18535 <18535@W10STAT-7-155.EDU.MARCONI>
2020-11-17 08:45:00 +0100

    DepositoBancario_01

---
+++ src/eser_cicli/DepositoBancario_01.java
@@ -0,0 +1,21 @@
+package eser_cicli;
+import java.util.Scanner;
+
+public class DepositoBancario_01 {
+    public static void main(String[] args) {
+        Scanner inp=new Scanner(System.in);
+        double saldo_iniz,perc_interess,soglia,anni=0;
+        System.out.println("inserire saldo iniziale");
+        saldo_iniz=inp.nextInt();
+        System.out.println("inserire percentuale d'interesse (solo numero)");
+        perc_interess=inp.nextInt();
+        System.out.println("inserire una soglia da raggiungere");
+        soglia=inp.nextInt();
+        while(saldo_iniz<soglia){
+            double percent=(saldo_iniz/100)*perc_interess;
+            saldo_iniz+=percent;
+            anni++;
+        }
+        System.out.println("per raggiungere la soglia di "+(int)soglia+" ci sono voluti "+(int)anni+" anni");
+    }
+}
